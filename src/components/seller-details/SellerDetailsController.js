ac34f92ccc1a4fd8e8f017319d4d3c195460393a
Ása Björk V <sa Bjrk V>
2016-03-11 00:13:14 +0000

    fetching seller details

--- src/components/seller-details/SellerDetailsController.js
+++ src/components/seller-details/SellerDetailsController.js
@@ -1,7 +1,22 @@
 "use strict";
 
 angular.module("project3App").controller("SellerDetailsController",
-function SellerDetailsController($scope, $rootScope, AppResource) {
+function SellerDetailsController($scope, $routeParams, AppResource) {
 
+
+
+	var sellerId = $routeParams.id;
+	$scope.sellerDetails = 'no details on this seller';
+
+	AppResource.getSellerDetails(parseInt(sellerId)) // TODO Should this be in ProductController ?? If so ...then how do 
+								// we make two controllers work on one html...or maybe it should be with the product card stuff
+		.success(function(data) {
+			console.log("SELLER DETAIL");
+			console.log(data);
+			$scope.sellerDetails = data;
+
+		}).error(function(){
+
+		});
 
 });
