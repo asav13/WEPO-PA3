e0a2516de7c6ad146e3af69a06828e3957b3ebf8
Ása Björk V <sa Bjrk V>
2016-03-11 00:30:00 +0000

    bug fixed in AppResource, added function to seller details

--- src/components/seller-details/SellerDetailsController.js
+++ src/components/seller-details/SellerDetailsController.js
@@ -1,11 +1,11 @@
 "use strict";
 
 angular.module("project3App").controller("SellerDetailsController",
+function SellerDetailsController($scope, $routeParams, AppResource) {
 
-function SellerDetailsController($scope, $rootScope, AppResource) {
+	/*Vedis*/
 	$scope.products = [];
 	var id = 1;
-
 
 	AppResource.getSellerProducts(id).success(function(data) {
 		console.log("DATA");
@@ -14,4 +14,21 @@ function SellerDetailsController($scope, $rootScope, AppResource) {
 	}).error(function() {
 		console.log("ERROR...");
 	});
+	/*End Vedis*/
+
+	/*Asa*/
+	var sellerId = $routeParams.id;
+	$scope.sellerDetails = 'no details on this seller';
+
+	AppResource.getSellerDetails(parseInt(sellerId)) // TODO Should this be in ProductController ?? If so ...then how do 
+								// we make two controllers work on one html...or maybe it should be with the product card stuff
+		.success(function(data) {
+			console.log("SELLER DETAIL");
+			console.log(data);
+			$scope.sellerDetails = data;
+
+		}).error(function(){
+			console.log("ERROR: Failed while fetching seller details.");
+		});
+	/*End Asa*/
 });
