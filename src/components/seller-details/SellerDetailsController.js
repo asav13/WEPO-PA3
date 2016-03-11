3556bf7019ad17b9deeceae792d9388ad7ceda36
vediserna <vedise13@ru.is>
2016-03-11 00:35:00 +0000

    bla

--- src/components/seller-details/SellerDetailsController.js
+++ src/components/seller-details/SellerDetailsController.js
@@ -3,13 +3,23 @@
 angular.module("project3App").controller("SellerDetailsController",
 function SellerDetailsController($scope, $rootScope, AppResource) {
 	$scope.products = [];
+	$scope.topTenProd = [];
 	var id = 1;
 
 
 	AppResource.getSellerProducts(id).success(function(data) {
+function bla(a, b) {
+						if(a.quantitySold < b.quantitySold) {
+							return -1;
+						} else {
+							return 1;
+						}}
+
 		console.log("DATA");
 		console.log(data);
 		$scope.products = data;
+					$scope.topTenProd = data;
+					$scope.topTenProd.sort(bla);
 	}).error(function() {
 		console.log("ERROR...");
 	});
