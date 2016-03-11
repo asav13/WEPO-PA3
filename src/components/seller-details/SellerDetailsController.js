7e41e78b46d279066f2ce435cd18c6358d396bbe
vediserna <vedise13@ru.is>
2016-03-11 00:06:09 +0000

    Working on tabs and seller page

--- src/components/seller-details/SellerDetailsController.js
+++ src/components/seller-details/SellerDetailsController.js
@@ -2,6 +2,17 @@
 
 angular.module("project3App").controller("SellerDetailsController",
 function SellerDetailsController($scope, $rootScope, AppResource) {
+	$scope.products = [];
+	var id = 1;
+
+
+	AppResource.getSellerProducts(id).success(function(data) {
+		console.log("DATA");
+		console.log(data);
+		$scope.products = data;
+	}).error(function() {
+		console.log("ERROR...");
+	});
 
 
 });
