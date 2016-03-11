6a6f1aaa10d064514fb300280a4a4e5cb2f16468
vediserna <vedise13@ru.is>
2016-03-11 01:08:13 +0000

    more tab work

--- src/components/seller-details/SellerDetailsController.js
+++ src/components/seller-details/SellerDetailsController.js
@@ -6,39 +6,33 @@ function SellerDetailsController($scope, $routeParams, AppResource) {
 	/*Vedis*/
 	$scope.products = [];
 	$scope.topTenProd = [];
-	var id = 1;
+	//var sellerId = $routeParams.id;
+	var sellerId = 3;
+	$scope.sellerDetails = 'no details on this seller';
 
-	AppResource.getSellerProducts(id).success(function(data) {
-function bla(a, b) {
-						if(a.quantitySold < b.quantitySold) {
-							return -1;
-						} else {
-							return 1;
-						}}
-
+	AppResource.getSellerProducts(sellerId).success(function(data) {
 		console.log("DATA");
 		console.log(data);
 		$scope.products = data;
-					$scope.topTenProd = data;
-					$scope.topTenProd.sort(bla);
+		$scope.topTenProd = FindTopTen(data);
 	}).error(function() {
 		console.log("ERROR...");
 	});
-	/*End Vedis*/
 
-	/*Asa*/
-	var sellerId = $routeParams.id;
-	$scope.sellerDetails = 'no details on this seller';
-
-	AppResource.getSellerDetails(parseInt(sellerId)) // TODO Should this be in ProductController ?? If so ...then how do 
-								// we make two controllers work on one html...or maybe it should be with the product card stuff
+	AppResource.getSellerDetails(parseInt(sellerId))
+	// TODO Should this be in ProductController ?? If so ...then how do 
+	// we make two controllers work on one html...or maybe it should be with the product card stuff
 		.success(function(data) {
-			console.log("SELLER DETAIL");
-			console.log(data);
+			//console.log("SELLER DETAIL");
+			//console.log(data);
 			$scope.sellerDetails = data;
 
 		}).error(function(){
 			console.log("ERROR: Failed while fetching seller details.");
 		});
-	/*End Asa*/
+
+
+	function FindTopTen(data) {
+
+	}
 });
