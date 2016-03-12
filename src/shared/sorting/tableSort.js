16f658bd90e3e12ebdcfe235fdd1a39a2b28aacc
Laura Orsini R Franca <lorsini.franca@gmail.com>
2016-03-12 01:21:00 +0000

    small changes

--- src/shared/sorting/tableSort.js
+++ src/shared/sorting/tableSort.js
@@ -56,7 +56,6 @@ angular.module("sharedServices").directive("tableSort", function () {
 		function initClickHandlers() {
 			angular.forEach(tableChildren, function(value) {
 				var el = angular.element(value);
-				console.log("value is: " + el)
 				var columnName = el.attr("columnName") || el.attr("data-columnName");
 
 				// Only attach a click handler if:
